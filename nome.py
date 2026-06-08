41d5abd6269df102e0aa62645ca4d24468dce6ba
Pamela <fagundespamela75@gmail.com>
2026-06-08 11:23:45 -0300

    Init: Primeiros passo em python

---
+++ nome.py
@@ -0,0 +1,22 @@
+# Vamos usar uma variavel chamada nome para
+# Guardar o nome do cliente. Utilizaremos tambem
+# O comando input(in -> dentro | put -> por em algum lugar)
+nome = input("Digite o seu nome: ")
+print("Olá, Sr(a)."+nome)
+print(f"Olá, Sr(a). {nome}")  
+
+  
+    #não é necessario colocar quantidade de letras 
+    #Sempre apertar o play para o terminal funcionar
+
+    # O operador +(mais) 
+    # foi utilizado para concatenar (juntar)
+    # o texto entre aspas ("") com a variavel nome
+
+    # A baixo, usamos o comando print com a letra
+    # f(format) paea inserir uma variavel no meio 
+    # de uma string. A variavel foi inserida com chaves ({})
+    # esta tecnica é chamada de interpolação
+
+print("Olá, Sr(a)."+nome+". Seja bem vindo")
+print(f"Olá, Sr(a). {nome}. Seja bem vindo") 
---
+++ operacoes.py
@@ -0,0 +1,21 @@
+n1 = input("Digite um numero: ")
+n2 = input("Digite outro numero: ")
+
+# Para realizar as operações aritmeticas, será 
+# necessario converter as variaveis n1 e n2 e em 
+# tipos numericos, que podem ser: int ou float 
+
+n1 = int(n1)
+n2 = int(n2)
+
+
+soma = n1 + n2
+subtrair = n1 - n2
+multiplicar = n1 * n2
+dividir = n1 / n2
+print(f"A soma entre os numeros {n1} e {n2} é {soma}")
+print(f"A soma entre os numeros {n1} e {n2} é {subtrair}")
+print(f"A soma entre os numeros {n1} e {n2} é {multiplicar}")
+print(f"A soma entre os numeros {n1} e {n2} é {dividir}")
+
+
